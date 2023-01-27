43529af229ae1ac8a9e1f4871ae18df24532d18b
jamesgade <gadejames48@gmail.com>
2023-01-27 11:25:29 +0530

    manage state with redux

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
 import { Component } from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import About from "./components/About";
 import Footer from "./components/Footer";
+import { connect } from "react-redux";
+import { addNewTask, deleteTask, toggleTaskReminder } from './redux/tasks/taskActions';
+import PropTypes from 'prop-types';
 
 class App extends Component {
 
@@ -12,87 +15,71 @@ class App extends Component {
 		super(props);
 
 		this.state = {
-			tasks: [
-				{
-					id: 1,
-					description: 'Dentist Appointment',
-					date: 'Jan 25th at 11:30AM',
-					reminder: true
-				},
-				{
-					id: 2,
-					description: 'Anniversery',
-					date: 'Jan 22th at 1:30AM',
-					reminder: false
-				},
-				{
-					id: 3,
-					description: 'Republic Day',
-					date: 'Jan 26th',
-					reminder: true
-				},
-			],
 			showAddForm: false
 		}
-
-		this.deleteTask = this.deleteTask.bind(this);
-	}
-
-	addTask = (task) => {
-		const id = Math.floor(Math.random() * 10000) + 1;
-		const newTask = { id, ...task }
-		this.setState((prevState) => ({
-			tasks: [newTask, ...prevState.tasks]
-		}))
-	}
-
-	deleteTask = (id) => {
-		this.setState((prevState) => ({
-			tasks: prevState.tasks.filter(task => task.id !== id)
-		}))
-	}
-
-	toggleReminder = (id) => {
-		this.setState((prevState) => ({
-			tasks: prevState.tasks.map(task => task.id === id ? { ...task, reminder: !task.reminder } : task)
-		}))
 	}
 
 	render() {
+
+		const { tasks, addNewTask, toggleTaskReminder, deleteTask } = this.props;
+
 		return (
-			<BrowserRouter>
-				<div className="container">
-					<Header
-						title='CRUD-JS'
-						showForm={() => this.setState({ showAddForm: !this.state.showAddForm })}
-						showAddForm={this.state.showAddForm}
-					/>
-					<Routes>
-						<Route path="/" exact element={
-							<>
-								{this.state.showAddForm && <AddTask onAdd={this.addTask} />}
-								{this.state.tasks.length > 0
-									? <Tasks
-										onDelete={this.deleteTask}
-										tasks={this.state.tasks}
-										onToggle={this.toggleReminder}
-									/>
-									: 'No tasks found click "Add" to create a task'
-								}
-								<Footer goto="about" label="Go to About" />
-							</>
-						} />
-						<Route path="about" element={<About />} />
-						<Route path="*" element={
-							<>
-								<h4>Page not found</h4>
-							</>
-						} />
-					</Routes>
-				</div>
-			</BrowserRouter>
+
+			<div className="container">
+				<Header
+					title='CRUD-JS'
+					showForm={() => this.setState({ showAddForm: !this.state.showAddForm })}
+					showAddForm={this.state.showAddForm}
+				/>
+				<Routes>
+					<Route path="/" element={
+						<>
+							{this.state.showAddForm && <AddTask onAdd={addNewTask} />}
+							{tasks.length > 0
+								? <Tasks
+									onDelete={deleteTask}
+									tasks={tasks}
+									onToggle={toggleTaskReminder}
+								/>
+								: 'No tasks found click "Add" to create a task'
+							}
+							<Footer goto="/about" label="Go to About" />
+						</>
+					} />
+					<Route path="/about" element={<About />} />
+					<Route path="*" element={
+						<>
+							<h4>Page not found</h4>
+						</>
+					} />
+				</Routes>
+			</div>
 		)
 	}
 }
 
-export default App;
+const mapStateToProps = state => {
+	return {
+		tasks: state.tasks.tasks
+	}
+}
+
+const mapDispatchToProps = dispatch => {
+	return {
+		addNewTask: (task) => dispatch(addNewTask(task)),
+		toggleTaskReminder: (id) => dispatch(toggleTaskReminder(id)),
+		deleteTask: (id) => dispatch(deleteTask(id))
+	}
+}
+
+App.propTypes = {
+	tasks: PropTypes.array.isRequired,
+	addNewTask: PropTypes.func.isRequired,
+	deleteTask: PropTypes.func.isRequired,
+	toggleTaskReminder: PropTypes.func.isRequired
+}
+
+export default connect(
+	mapStateToProps,
+	mapDispatchToProps
+)(App);
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,20 @@
 import { Component } from "react";
 import PropTypes from 'prop-types';
 import Button from "./Button";
+import withRouter from "../hoc/withRouter";
 
 class Header extends Component {
 
     render() {
+        const { router } = this.props;
         return (
             <header className="header">
                 <h1>{this.props.title}</h1>
-                <Button
+                {router.location.pathname !== '/about' && <Button
                     text={`${this.props.showAddForm ? 'Close' : 'Add'}`}
                     color={`${this.props.showAddForm ? 'red' : 'green'}`}
                     onClick={this.props.showForm}
-                />
+                />}
             </header>
         )
     }
@@ -24,6 +26,8 @@ Header.defaultProps = {
 
 Header.propTypes = {
     title: PropTypes.string.isRequired,
+    showForm: PropTypes.func.isRequired,
+    showAddForm: PropTypes.bool.isRequired
 }
 
-export default Header;
+export default withRouter(Header);
